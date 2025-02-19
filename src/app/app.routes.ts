7a6412fdb5064b5d9a8148834746fe1cce01dd72
Martin <martin.occhi@kavak.com>
2025-02-19 18:22:42 -0300

    feat: added practice-areas page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { PracticeAreasComponent } from './practice-areas/practice-areas.component';
 export const routes: Routes = [
   { path: 'home', title: 'Home', component: HomeComponent },
   { path: 'about-us', title: 'About Us', component: AboutUsComponent },
+  { path: 'practice-areas', title: 'About Us', component: PracticeAreasComponent },
   { path: '**', title: 'Home', component: HomeComponent },
 ];
