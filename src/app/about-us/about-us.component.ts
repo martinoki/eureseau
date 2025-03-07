4b862083d542eededbb6bbefe211b502738f490b
Martin <martin.occhi@kavak.com>
2025-03-06 22:36:26 -0300

    feat: added missing section in about us and enhanced pics

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -22,24 +22,29 @@ export class AboutUsComponent {
     {
       nombre: 'Dr. Jorge Eduardo Pedreira',
       cargo: 'Titular y fundador del estudio',
-      imagen: 'papa_tere.png',
+      imagen: 'papa_tere_enhanced.png',
       description:
         'En el área financiera y bancaria, nuestro estudio ofrece asesoría integral a entidades financieras, empresas y particulares. Nos especializamos en la estructuración de préstamos, financiamiento corporativo, garantías, reestructuración de deudas y cumplimiento normativo. Nuestro equipo de abogados cuenta con una vasta experiencia en la negociación de contratos financieros, así como en la resolución de disputas derivadas de estas operaciones. Además, brindamos apoyo en procesos regulatorios y ante organismos de control. Trabajamos en estrecha colaboración con nuestros clientes para asegurar soluciones efectivas, personalizadas y en cumplimiento con la legislación vigente.',
     },
     {
       nombre: 'Dra. Maria Teresa Pedreira',
       cargo: 'Especialista en derecho laboral y asesoramiento de empresas',
-      imagen: 'mama_tere.png',
+      imagen: 'tere_enhanced.png',
       description:
         'En el área financiera y bancaria, nuestro estudio ofrece asesoría integral a entidades financieras, empresas y particulares. Nos especializamos en la estructuración de préstamos, financiamiento corporativo, garantías, reestructuración de deudas y cumplimiento normativo. Nuestro equipo de abogados cuenta con una vasta experiencia en la negociación de contratos financieros, así como en la resolución de disputas derivadas de estas operaciones. Además, brindamos apoyo en procesos regulatorios y ante organismos de control. Trabajamos en estrecha colaboración con nuestros clientes para asegurar soluciones efectivas, personalizadas y en cumplimiento con la legislación vigente.',
     },
     {
       nombre: 'Dra. Teresa E. Felix de Pedreira',
       cargo: 'Doctora en derecho societario',
-      imagen: 'tere.png',
+      imagen: 'mama_tere_enhanced.png',
       description:
         'En el área financiera y bancaria, nuestro estudio ofrece asesoría integral a entidades financieras, empresas y particulares. Nos especializamos en la estructuración de préstamos, financiamiento corporativo, garantías, reestructuración de deudas y cumplimiento normativo. Nuestro equipo de abogados cuenta con una vasta experiencia en la negociación de contratos financieros, así como en la resolución de disputas derivadas de estas operaciones. Además, brindamos apoyo en procesos regulatorios y ante organismos de control. Trabajamos en estrecha colaboración con nuestros clientes para asegurar soluciones efectivas, personalizadas y en cumplimiento con la legislación vigente.',
     },
+  ];
+
+  eureseauText = [
+    'Nuestra misión es ofrecer asesoría y representación legal de primer nivel para abordar los desafíos y las oportunidades en el entorno laboral. Con una sólida experiencia en derecho laboral, nuestro equipo de abogados se especializa en proporcionar soluciones estratégicas y personalizadas en temas como conflictos laborales, negociaciones colectivas, y protección de derechos de los trabajadores.',
+    'Nos enorgullece nuestra capacidad para combinar un profundo conocimiento jurídico con un enfoque humano y comprometido, garantizando que cada cliente reciba la atención y el respaldo necesarios para alcanzar sus objetivos. En Eureseau, su tranquilidad y éxito en el ámbito laboral son nuestra prioridad.',
   ];
 
   selectedTitular = this.titulares[0];
