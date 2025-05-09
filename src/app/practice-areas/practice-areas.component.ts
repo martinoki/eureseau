5ce839cf2a0cb857a8f51be9adf863c493df019f
Martin <martin.occhi@kavak.com>
2025-05-09 09:38:12 -0300

    feat: added practice areas

--- src/app/practice-areas/practice-areas.component.ts
+++ src/app/practice-areas/practice-areas.component.ts
@@ -69,12 +69,62 @@ export class PracticeAreasComponent {
     },
     {
       selected: false,
-      image: 'practice-areas/building.png',
-      title: 'AREA CONTENCIOSA EJECUTIVA EN PROPIEDAD HORIZONTAL',
+      image: 'practice-areas/briefcase.png',
+      title: 'DERECHO LABORAL',
+      description: [
+        {
+          type: 'text',
+          text: 'Contamos con amplia trayectoria asesorando empresas y clientes en asuntos laborales,',
+        },
+        {
+          type: 'bullet',
+          text: 'Celebración de acuerdos de desvinculación, reorganizaciones, programas preventivos de crisis,',
+        },
+        {
+          type: 'bullet',
+          text: 'Armado de estructuras en capital humano acorde a legislación vigente,',
+        },
+        {
+          type: 'bullet',
+          text: 'Asuntos contenciosos, solución de controversias mediante procedimientos conciliatorios,',
+        },
+        {
+          type: 'bullet',
+          text: 'Cursos de capacitación en el área de capital humano empresarial,',
+        },
+        {
+          type: 'bullet',
+          text: 'Despidos, indemnizaciones, accidentes de trabajo.',
+        },
+      ],
+    },
+    {
+      selected: false,
+      image: 'practice-areas/shop.png',
+      title: 'COMERCIAL',
       description: [
         {
           type: 'bullet',
-          text: 'Asesoramiento en materia sucesoria, Testamentaria, ab intestato, cesiones de derechos hereditarios.',
+          text: 'Cartera morosa: Cobranza judicial o extrajudicial, recuperos y refinanciaciones. Control de gestión. Auditorías legales. Seguimiento.',
+        },
+        {
+          type: 'bullet',
+          text: 'Gestión judicial: Asesoramiento y defensa en litigios en que la empresa sea parte reclamante, demandada o vinculada.',
+        },
+        {
+          type: 'text',
+          text: 'Concursos y quiebras: Procedimiento ante deudores quebrados o concursados, verficación de créditos.',
+        },
+      ],
+    },
+    {
+      selected: false,
+      image: 'practice-areas/paper.png',
+      title: 'SUCESIONES',
+      description: [
+        {
+          type: 'bullet',
+          text: 'Asesoramiento en materia sucesoria, testamentaria, ab intestato, cesiones de derechos hereditarios.',
         },
         {
           type: 'bullet',
@@ -92,34 +142,162 @@ export class PracticeAreasComponent {
     },
     {
       selected: false,
-      image: 'practice-areas/medal.png',
-      title: 'CONCURSOS Y QUIEBRAS',
+      image: 'practice-areas/building.png',
+      title: 'PROPIEDAD HORIZONTAL',
       description: [
         {
+          type: 'text',
+          text: 'Nuestras oficinas poseen un área dedicada en forma personalizada a la atención de problemas jurídicos derivados de las relaciones consorciales en la Propiedad Horizontal. Ello comprende asesoramiento en todo lo que implica el órgano deliberativo (Asamblea) como administrativo,  relaciones laborales, consultas, gestiones de cobro extrajudiciales  y ejecuciones de expensas. Con mas de 100 Consorcio como clientes, brindando en forma continua la recuperación de las deudas de expensas.',
+        },
+        {
           type: 'bullet',
-          text: 'Constitución de sociedades comerciales. Inscripción de sociedades extranjeras. Reforma de estatutos, empresas vinculadas. Emisión de obligaciones subordinadas, condiciones, procesos de emisión.',
+          text: 'Búsqueda de bienes.',
+        },
+        {
+          type: 'bullet',
+          text: 'Sucesiones en el extranjero.',
+        },
+        {
+          type: 'bullet',
+          text: 'Liquidación de bienes.',
         },
       ],
     },
     {
       selected: false,
-      image: 'practice-areas/hammer.png',
-      title: 'ASPECTOS CONTENCIOSOS EN MATERIA LABORAL, COMERCIAL Y CIVIL',
+      image: 'practice-areas/justice.png',
+      title: 'DAÑOS Y PERJUICIOS',
       description: [
         {
           type: 'bullet',
-          text: 'Constitución de sociedades comerciales. Inscripción de sociedades extranjeras. Reforma de estatutos, empresas vinculadas. Emisión de obligaciones subordinadas, condiciones, procesos de emisión.',
+          text: 'Daños en materia contractual y extracontractual.',
+        },
+        {
+          type: 'bullet',
+          text: 'Accidentes de tránsito.',
+        },
+        {
+          type: 'bullet',
+          text: 'Evaluación y cuantificación del daño con especialistas.',
+        },
+        {
+          type: 'bullet',
+          text: 'Peritos de parte y Consultores Técnicos según sea la naturaleza del daño.',
         },
       ],
     },
     {
       selected: false,
-      image: 'practice-areas/paper.png',
-      title: 'DERECHO SUCESORIO',
+      image: 'practice-areas/build.png',
+      title: 'DERECHO PENAL',
+      description: [
+        {
+          type: 'text',
+          text: 'El estudio cuenta con abogados penalistas especializados.',
+        },
+      ],
+    },
+    {
+      selected: false,
+      image: 'practice-areas/hand_gear.png',
+      title: 'SERVICIOS NOTORIALES',
       description: [
         {
           type: 'bullet',
-          text: 'Constitución de sociedades comerciales. Inscripción de sociedades extranjeras. Reforma de estatutos, empresas vinculadas. Emisión de obligaciones subordinadas, condiciones, procesos de emisión.',
+          text: 'Constitución y redacción de estatutos de sociedades (nacionales y extranjeras), cesión de cuotas, incorporación, poderes.',
+        },
+        {
+          type: 'bullet',
+          text: 'Constitución de derechos reales e inmobiliarios, compraventas, donaciones, usufructos.',
+        },
+        {
+          type: 'bullet',
+          text: 'Certificaciones y legalizaciones.',
+        },
+        {
+          type: 'bullet',
+          text: 'Estudio de títulos, transmisiones de dominio, hipotecas, garantías.',
+        },
+        {
+          type: 'bullet',
+          text: 'Contratos fiduciarios, poderes, representaciones, testamentos, tasaciones.',
+        },
+      ],
+    },
+    {
+      selected: false,
+      image: 'practice-areas/sheet.png',
+      title: 'ASPECTOS GENERALES',
+      description: [
+        {
+          type: 'bullet',
+          text: 'Información estratégica y políticas generales de inversiones extranjeras: Mercosur, C.E.E.',
+        },
+        {
+          type: 'bullet',
+          text: 'Representación en Consejos de Administración, Directorios, Juntas, etc.',
+        },
+        {
+          type: 'bullet',
+          text: 'Arbitraje y mediación.',
+        },
+        {
+          type: 'bullet',
+          text: 'Derecho energético.',
+        },
+        {
+          type: 'bullet',
+          text: 'Propiedad Intelectual.',
+        },
+      ],
+    },
+    {
+      selected: false,
+      image: 'practice-areas/global.png',
+      title: 'MEDIO AMBIENTE',
+      description: [
+        {
+          type: 'text',
+          text: 'Asesoría legal y técnica en materia de derecho ambiental.',
+        },
+        {
+          type: 'bullet',
+          text: 'Asesoramiento en la obtención de permisos, autorizaciones y licencias ambientales.',
+        },
+        {
+          type: 'bullet',
+          text: 'Elaboración de estudios de impacto ambiental.',
+        },
+        {
+          type: 'bullet',
+          text: 'Defensa ante los tribunales.',
+        },
+        {
+          type: 'bullet',
+          text: 'Planificación ambiental.',
+        },
+        {
+          type: 'bullet',
+          text: 'Solución de controversias en la vía administrativa ambiental.',
+        },
+      ],
+    },
+    {
+      selected: false,
+      image: 'practice-areas/folder.png',
+      title: 'CONTENCIOSO ADMINISTRATIVO',
+      description: [
+        {
+          type: 'text',
+          text: 'Asesoramiento integral en todo tipo de cuestiones relacionadas con el Derecho Administrativo en todas sus instancias y jurisdicciones.',
+        },
+        {
+          type: 'text',
+          text: 'Nuestro servicio comprende el análisis y encuadre legal de todo tipo de casos desde la sede administrativa.',
+        },
+        {
+          type: 'text',
+          text: 'Comprende la interposición de reclamos y recursos administrativos, así como la representación y patrocinio en acciones contencioso administrativas.',
         },
       ],
     },
